8787994f89493245efdd4830b08fab0e368adc26
Bharat Nagandla <bharath2232@gmail.com>
2019-05-19 11:01:25 +0200

    finished

--- sireneApi/index.js
+++ sireneApi/index.js
@@ -2,8 +2,20 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const db = require('./dbconfig')
-const sirene = require('./model')
+const sirene = require('./model');
+const Token = require('./tokenModel');
+const jwt = require('jsonwebtoken');
+
 mongoose.Promise =global.Promise;
+
+
+
+handleToken = (token) => {
+    const newtoken = jwt.sign({ foo: 'bar' }, 'shhhhh');
+    return newtoken
+}
+
+console.log('token');
 
 mongoose.connect(db.url,{userNewUrlParser : true}).then(()=> {
     console.log('MongoDB Connected')
@@ -20,16 +32,36 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 // define a simple route
-app.get('/:id', (req, res) => {
-    const sevran = sirene.findOne({'fields.siren':req.params.id})
-    .then(result => {
-        console.log('Sucess',result);
-        res.send(result);
-    })
-        .catch(err => console.log('error duude', err) )
+app.get('/fetch', (req, res) => {
+    Token.findOne({token:req.query.key})
+        .then(result => {
+            if (result){
+                sirene.findOne({'fields.siren':req.query.id})
+                    .then(result => {
+                        console.log('Sucess',result);
+                        res.send(result);
+                    })
+                    .catch(err => console.log('error duude', err) )
+            } else {
+                res.send('invalid token Please get a token ')
+            }
+        })
+    console.log('auth',auth)
+
+
+
+
 
 })
+app.post('/token', (req, res) => {
+   const token = new Token({token:handleToken()});
+    token.save()
+        .then(result => {
+            res.send(`You token is ${result.token} successfully generated, Please do not Regenerate again` )
+        })
+        .catch(err => console.log(err,'somthing is wrong'))
 
+})
 // listen for requests
 app.listen(3000, () => {
     console.log("Server is listening on port 3000");
